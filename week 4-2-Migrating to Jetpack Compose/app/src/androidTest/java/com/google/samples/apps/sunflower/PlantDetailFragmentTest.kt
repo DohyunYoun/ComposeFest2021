fe0fab869b739af789d69f97e5ea996212419e02
ellen.youn <ellen.youn@kakaocorp.com>
2021-12-09 22:09:43 +0900

    week 4 : migration (test)

--- week 4-2-Migrating to Jetpack Compose/app/src/androidTest/java/com/google/samples/apps/sunflower/PlantDetailFragmentTest.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/androidTest/java/com/google/samples/apps/sunflower/PlantDetailFragmentTest.kt
@@ -20,6 +20,9 @@ import android.accessibilityservice.AccessibilityService
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.Navigation.findNavController
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
@@ -48,18 +51,20 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class PlantDetailFragmentTest {
 
+    /**
+     * compose test
+     */
     @Rule
     @JvmField
-    val activityTestRule = ActivityScenarioRule(GardenActivity::class.java)
+    val composeTestRule = createAndroidComposeRule<GardenActivity>()
 
-    // Note that keeping these references is only safe if the activity is not recreated.
     private lateinit var activity: ComponentActivity
 
     @Before
     fun jumpToPlantDetailFragment() {
         populateDatabase()
 
-        activityTestRule.scenario.onActivity { gardenActivity ->
+        composeTestRule.activityRule.scenario.onActivity { gardenActivity ->
             activity = gardenActivity
 
             val bundle = Bundle().apply { putString("plantId", "malus-pumila") }
@@ -69,9 +74,34 @@ class PlantDetailFragmentTest {
 
     @Test
     fun testPlantName() {
-        onView(ViewMatchers.withText("Apple"))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        composeTestRule.onNodeWithText("Apple").assertIsDisplayed()
     }
+
+
+//    @Rule
+//    @JvmField
+//    val activityTestRule = ActivityScenarioRule(GardenActivity::class.java)
+//
+//    // Note that keeping these references is only safe if the activity is not recreated.
+//    private lateinit var activity: ComponentActivity
+//
+//    @Before
+//    fun jumpToPlantDetailFragment() {
+//        populateDatabase()
+//
+//        activityTestRule.scenario.onActivity { gardenActivity ->
+//            activity = gardenActivity
+//
+//            val bundle = Bundle().apply { putString("plantId", "malus-pumila") }
+//            findNavController(activity, R.id.nav_host).navigate(R.id.plant_detail_fragment, bundle)
+//        }
+//    }
+//
+//    @Test
+//    fun testPlantName() {
+//        onView(ViewMatchers.withText("Apple"))
+//            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+//    }
 
     @Test
     fun testShareTextIntent() {
